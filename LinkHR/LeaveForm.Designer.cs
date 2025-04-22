01f5bcf1b63afc65ccd2aac1736df1ec51480fe6
FathimaNafra <fathumanafra5@gmail.com>
2025-04-22 14:33:30 +0530

    add textbox,comboBox,buttons to labels(leaves form)

--- LinkHR/LeaveForm.Designer.cs
+++ LinkHR/LeaveForm.Designer.cs
@@ -37,6 +37,20 @@
             label7 = new Label();
             label8 = new Label();
             label9 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label10 = new Label();
+            label11 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // label1
@@ -121,11 +135,136 @@
             label9.TabIndex = 8;
             label9.Text = "Reason";
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(300, 61);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(177, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(299, 95);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(248, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Dep A ", "Dep B", "Dep C", "DEp D" });
+            comboBox1.Location = new Point(301, 139);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Casual ", "Sick", "Half Day" });
+            comboBox2.Location = new Point(303, 178);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 12;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(303, 219);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 13;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(304, 259);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(250, 27);
+            dateTimePicker2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(303, 297);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(304, 335);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(250, 106);
+            textBox4.TabIndex = 16;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(91, 474);
+            label10.Name = "label10";
+            label10.Size = new Size(118, 20);
+            label10.TabIndex = 17;
+            label10.Text = "Contact Number";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(95, 512);
+            label11.Name = "label11";
+            label11.Size = new Size(46, 20);
+            label11.TabIndex = 18;
+            label11.Text = "Email";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(308, 474);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(183, 27);
+            textBox5.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(309, 515);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(359, 27);
+            textBox6.TabIndex = 20;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(358, 602);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 49);
+            button1.TabIndex = 21;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(678, 602);
+            button2.Name = "button2";
+            button2.Size = new Size(142, 49);
+            button2.TabIndex = 22;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // LeaveForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1153, 713);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(label9);
             Controls.Add(label8);
             Controls.Add(label7);
@@ -152,5 +291,19 @@
         private Label label7;
         private Label label8;
         private Label label9;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label10;
+        private Label label11;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Button button1;
+        private Button button2;
     }
 }
