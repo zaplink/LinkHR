42d97b317a1c755d253ca2421223c9819522fa07
Sitharahansamali <hsithara69@gmail.com>
2025-04-22 15:17:59 +0530

    add more tools to forms(register new employee)

--- LinkHR/AddEmployeeForm.cs
+++ LinkHR/AddEmployeeForm.cs
@@ -21,5 +21,20 @@ namespace LinkHR
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
