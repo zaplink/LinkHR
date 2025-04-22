44dfbfd606731c4696a96dcb688b4d98b2f58955
FathimaNafra <fathumanafra5@gmail.com>
2025-04-22 14:10:21 +0530

    add labels(leaves form)

--- LinkHR/LeaveForm.Designer.cs
+++ LinkHR/LeaveForm.Designer.cs
@@ -29,24 +29,111 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(64, 57);
             label1.Name = "label1";
-            label1.Size = new Size(151, 28);
+            label1.Size = new Size(98, 20);
             label1.TabIndex = 0;
-            label1.Text = "Leave form here";
-            label1.Click += label1_Click;
+            label1.Text = " Employee ID\r\n";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(72, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Employee Name";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(73, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Department";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(73, 172);
+            label4.Name = "label4";
+            label4.Size = new Size(80, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Leave type";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(73, 215);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 20);
+            label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(72, 215);
+            label6.Name = "label6";
+            label6.Size = new Size(79, 20);
+            label6.TabIndex = 5;
+            label6.Text = "From Date";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(72, 255);
+            label7.Name = "label7";
+            label7.Size = new Size(61, 20);
+            label7.TabIndex = 6;
+            label7.Text = "To Date";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(81, 294);
+            label8.Name = "label8";
+            label8.Size = new Size(78, 20);
+            label8.TabIndex = 7;
+            label8.Text = "Total Days";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(84, 327);
+            label9.Name = "label9";
+            label9.Size = new Size(57, 20);
+            label9.TabIndex = 8;
+            label9.Text = "Reason";
             // 
             // LeaveForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1153, 713);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
             Name = "LeaveForm";
             Text = "LeaveForm";
@@ -57,5 +144,13 @@
         #endregion
 
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
     }
 }
