4a3d35436530af3025ee80ff6035f7a0891d5715
Sitharahansamali <hsithara69@gmail.com>
2025-04-23 09:54:23 +0530

    Create Remove Employee Form

--- LinkHR/RemoveEmployeeForm.Designer.cs
+++ LinkHR/RemoveEmployeeForm.Designer.cs
@@ -29,25 +29,76 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            textBox2 = new TextBox();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(433, 34);
             label1.Name = "label1";
-            label1.Size = new Size(326, 28);
+            label1.Size = new Size(133, 20);
             label1.TabIndex = 0;
-            label1.Text = "Able to remove exsisting employees";
+            label1.Text = "Remove Employee";
             // 
-            // RemoveEmployee
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(87, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(145, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Search By ID/Name :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(282, 110);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(269, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(602, 110);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(371, 197);
+            label3.Name = "label3";
+            label3.Size = new Size(125, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Employee Details";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(301, 237);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(289, 183);
+            textBox2.TabIndex = 5;
+            // 
+            // RemoveEmployeeForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1011, 596);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
             Controls.Add(label1);
-            Name = "RemoveEmployee";
+            Name = "RemoveEmployeeForm";
             Text = "RemoveEmployee";
             ResumeLayout(false);
             PerformLayout();
@@ -56,5 +107,10 @@
         #endregion
 
         private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox2;
     }
 }
