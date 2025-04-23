ad93b590c3b6111b859fb460d6f1523106b63ab8
ThakshalaMadhuwanthi <hgtmadhuwanthi@gmail.com>
2025-04-23 09:27:52 +0530

    Create Reports

--- LinkHR/ReportsForm.cs
+++ LinkHR/ReportsForm.cs
@@ -10,11 +10,52 @@ using System.Windows.Forms;
 
 namespace LinkHR
 {
-    public partial class ReportsForm: Form
+    public partial class ReportsForm : Form
     {
         public ReportsForm()
         {
             InitializeComponent();
+        }
+
+        private Form activeForm = null;
+
+        private void loadform(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+
+            reportPanel.Controls.Add(childForm);
+            reportPanel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    loadform(new ReportAttendence());
+                    break;
+                case 1:
+                    loadform(new ReportPayroll());
+                    break;
+                default:
+                    MessageBox.Show("Invalid selection");
+                    break;
+            }
+
+        }
+
+        private void ReportsForm_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;  // Selects the first item
         }
     }
 }
