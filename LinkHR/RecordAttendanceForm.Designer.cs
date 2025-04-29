9bbc29cc32fc39986111f2e80a1d728a536d77b0
pathummadhusanka <commits@pathummadhusanka.com>
2025-04-29 16:52:28 +0100

    feat(attendance): add employee search box

--- LinkHR/RecordAttendanceForm.Designer.cs
+++ LinkHR/RecordAttendanceForm.Designer.cs
@@ -38,7 +38,18 @@
             CheckInTime = new DateTimePicker();
             CheckOutTime = new DateTimePicker();
             RecordAttendanceBtn = new Button();
-            dbtestbtn = new Button();
+            EmployeeListBox = new ListBox();
+            label5 = new Label();
+            EmpIdLbl = new Label();
+            EmpFirstNameLbl = new Label();
+            EmpLastNameLbl = new Label();
+            EmpEmailLbl = new Label();
+            EmpPhoneLbl = new Label();
+            EmpEmailFill = new Label();
+            EmpPhoneFill = new Label();
+            EmpLastNameFill = new Label();
+            EmpFirstNameFill = new Label();
+            EmpIdFill = new Label();
             empIdInp = new TextBox();
             SuspendLayout();
             // 
@@ -47,100 +58,218 @@
             empIdInp.Location = new Point(163, 40);
             empIdInp.Margin = new Padding(3, 4, 3, 4);
             empIdInp.Name = "empIdInp";
-            empIdInp.Size = new Size(193, 27);
+            empIdInp.Size = new Size(193, 23);
             empIdInp.TabIndex = 3;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(40, 112);
+            label1.Location = new Point(425, 18);
             label1.Name = "label1";
-            label1.Size = new Size(48, 20);
+            label1.Size = new Size(37, 15);
             label1.TabIndex = 0;
             label1.Text = "Date :";
             // 
-            // Date
+            // DatePicker
             // 
-            DatePicker.Location = new Point(103, 78);
-            DatePicker.Name = "Date";
-            DatePicker.Size = new Size(200, 23);
+            DatePicker.Location = new Point(516, 12);
+            DatePicker.Margin = new Padding(3, 2, 3, 2);
+            DatePicker.Name = "DatePicker";
+            DatePicker.Size = new Size(176, 23);
             DatePicker.TabIndex = 1;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(41, 51);
+            label2.Location = new Point(12, 18);
             label2.Name = "label2";
-            label2.Size = new Size(82, 20);
+            label2.Size = new Size(65, 15);
             label2.TabIndex = 2;
             label2.Text = "Employee :";
             // 
             // EmpIdTxt
             // 
-            EmpIdTxt.Location = new Point(143, 30);
+            EmpIdTxt.Location = new Point(97, 15);
+            EmpIdTxt.Margin = new Padding(3, 2, 3, 2);
             EmpIdTxt.Name = "EmpIdTxt";
-            EmpIdTxt.Size = new Size(169, 23);
+            EmpIdTxt.Size = new Size(260, 23);
             EmpIdTxt.TabIndex = 3;
-            //             // label3
+            EmpIdTxt.Click += EmpIdTxt_Click;
+            EmpIdTxt.TextChanged += EmpIdTxt_TextChanged;
+            // 
+            // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(41, 172);
+            label3.Location = new Point(425, 66);
             label3.Name = "label3";
-            label3.Size = new Size(77, 20);
+            label3.Size = new Size(64, 15);
             label3.TabIndex = 4;
             label3.Text = "Check-In : ";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(41, 229);
+            label4.Location = new Point(425, 121);
             label4.Name = "label4";
-            label4.Size = new Size(89, 20);
+            label4.Size = new Size(74, 15);
             label4.TabIndex = 5;
             label4.Text = "Check-Out : ";
             // 
             // CheckInTime
             // 
             CheckInTime.Format = DateTimePickerFormat.Time;
-            CheckInTime.Location = new Point(143, 121);
+            CheckInTime.Location = new Point(516, 60);
+            CheckInTime.Margin = new Padding(3, 2, 3, 2);
             CheckInTime.Name = "CheckInTime";
-            CheckInTime.Size = new Size(200, 23);
+            CheckInTime.Size = new Size(176, 23);
             CheckInTime.TabIndex = 6;
             // 
             // CheckOutTime
             // 
             CheckOutTime.Format = DateTimePickerFormat.Time;
-            CheckOutTime.Location = new Point(143, 164);
+            CheckOutTime.Location = new Point(516, 115);
+            CheckOutTime.Margin = new Padding(3, 2, 3, 2);
             CheckOutTime.Name = "CheckOutTime";
-            CheckOutTime.Size = new Size(200, 23);
+            CheckOutTime.Size = new Size(176, 23);
             CheckOutTime.TabIndex = 7;
             // 
             // RecordAttendanceBtn
             // 
-            RecordAttendanceBtn.Location = new Point(36, 232);
+            RecordAttendanceBtn.Location = new Point(425, 204);
+            RecordAttendanceBtn.Margin = new Padding(3, 2, 3, 2);
             RecordAttendanceBtn.Name = "RecordAttendanceBtn";
-            RecordAttendanceBtn.Size = new Size(243, 23);
+            RecordAttendanceBtn.Size = new Size(267, 33);
             RecordAttendanceBtn.TabIndex = 8;
             RecordAttendanceBtn.Text = "Record Attendance";
             RecordAttendanceBtn.UseVisualStyleBackColor = true;
             RecordAttendanceBtn.Click += RecordAttendanceBtn_Click;
             // 
-            // dbtestbtn
+            // EmployeeListBox
             // 
-            dbtestbtn.Location = new Point(667, 39);
-            dbtestbtn.Margin = new Padding(3, 4, 3, 4);
-            dbtestbtn.Name = "dbtestbtn";
-            dbtestbtn.Size = new Size(86, 31);
-            dbtestbtn.TabIndex = 9;
-            dbtestbtn.Text = "DB";
-            dbtestbtn.UseVisualStyleBackColor = true;
+            EmployeeListBox.FormattingEnabled = true;
+            EmployeeListBox.ItemHeight = 15;
+            EmployeeListBox.Location = new Point(97, 40);
+            EmployeeListBox.Name = "EmployeeListBox";
+            EmployeeListBox.Size = new Size(260, 94);
+            EmployeeListBox.TabIndex = 10;
+            EmployeeListBox.MouseClick += EmployeeListBox_MouseClick;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 68);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Employee  Details";
+            // 
+            // EmpIdLbl
+            // 
+            EmpIdLbl.AutoSize = true;
+            EmpIdLbl.Location = new Point(40, 105);
+            EmpIdLbl.Name = "EmpIdLbl";
+            EmpIdLbl.Size = new Size(24, 15);
+            EmpIdLbl.TabIndex = 12;
+            EmpIdLbl.Text = "ID :";
+            // 
+            // EmpFirstNameLbl
+            // 
+            EmpFirstNameLbl.AutoSize = true;
+            EmpFirstNameLbl.Location = new Point(40, 137);
+            EmpFirstNameLbl.Name = "EmpFirstNameLbl";
+            EmpFirstNameLbl.Size = new Size(70, 15);
+            EmpFirstNameLbl.TabIndex = 13;
+            EmpFirstNameLbl.Text = "First Name :";
+            // 
+            // EmpLastNameLbl
+            // 
+            EmpLastNameLbl.AutoSize = true;
+            EmpLastNameLbl.Location = new Point(40, 173);
+            EmpLastNameLbl.Name = "EmpLastNameLbl";
+            EmpLastNameLbl.Size = new Size(69, 15);
+            EmpLastNameLbl.TabIndex = 14;
+            EmpLastNameLbl.Text = "Last Name :";
+            // 
+            // EmpEmailLbl
+            // 
+            EmpEmailLbl.AutoSize = true;
+            EmpEmailLbl.Location = new Point(40, 240);
+            EmpEmailLbl.Name = "EmpEmailLbl";
+            EmpEmailLbl.Size = new Size(42, 15);
+            EmpEmailLbl.TabIndex = 16;
+            EmpEmailLbl.Text = "Email :";
+            // 
+            // EmpPhoneLbl
+            // 
+            EmpPhoneLbl.AutoSize = true;
+            EmpPhoneLbl.Location = new Point(40, 204);
+            EmpPhoneLbl.Name = "EmpPhoneLbl";
+            EmpPhoneLbl.Size = new Size(69, 15);
+            EmpPhoneLbl.TabIndex = 15;
+            EmpPhoneLbl.Text = "Phone No. :";
+            // 
+            // EmpEmailFill
+            // 
+            EmpEmailFill.AutoSize = true;
+            EmpEmailFill.Location = new Point(139, 240);
+            EmpEmailFill.Name = "EmpEmailFill";
+            EmpEmailFill.Size = new Size(36, 15);
+            EmpEmailFill.TabIndex = 21;
+            EmpEmailFill.Text = "Email";
+            // 
+            // EmpPhoneFill
+            // 
+            EmpPhoneFill.AutoSize = true;
+            EmpPhoneFill.Location = new Point(139, 204);
+            EmpPhoneFill.Name = "EmpPhoneFill";
+            EmpPhoneFill.Size = new Size(63, 15);
+            EmpPhoneFill.TabIndex = 20;
+            EmpPhoneFill.Text = "Phone No.";
+            // 
+            // EmpLastNameFill
+            // 
+            EmpLastNameFill.AutoSize = true;
+            EmpLastNameFill.Location = new Point(139, 173);
+            EmpLastNameFill.Name = "EmpLastNameFill";
+            EmpLastNameFill.Size = new Size(63, 15);
+            EmpLastNameFill.TabIndex = 19;
+            EmpLastNameFill.Text = "Last Name";
+            // 
+            // EmpFirstNameFill
+            // 
+            EmpFirstNameFill.AutoSize = true;
+            EmpFirstNameFill.Location = new Point(139, 137);
+            EmpFirstNameFill.Name = "EmpFirstNameFill";
+            EmpFirstNameFill.Size = new Size(64, 15);
+            EmpFirstNameFill.TabIndex = 18;
+            EmpFirstNameFill.Text = "First Name";
+            // 
+            // EmpIdFill
+            // 
+            EmpIdFill.AutoSize = true;
+            EmpIdFill.Location = new Point(139, 105);
+            EmpIdFill.Name = "EmpIdFill";
+            EmpIdFill.Size = new Size(18, 15);
+            EmpIdFill.TabIndex = 17;
+            EmpIdFill.Text = "ID";
             // 
             // RecordAttendanceForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
-            Controls.Add(dbtestbtn);
+            ClientSize = new Size(800, 450);
+            Controls.Add(EmpEmailFill);
+            Controls.Add(EmpPhoneFill);
+            Controls.Add(EmpLastNameFill);
+            Controls.Add(EmpFirstNameFill);
+            Controls.Add(EmpIdFill);
+            Controls.Add(EmpEmailLbl);
+            Controls.Add(EmpPhoneLbl);
+            Controls.Add(EmpLastNameLbl);
+            Controls.Add(EmpFirstNameLbl);
+            Controls.Add(EmpIdLbl);
+            Controls.Add(label5);
+            Controls.Add(EmployeeListBox);
             Controls.Add(RecordAttendanceBtn);
             Controls.Add(CheckOutTime);
             Controls.Add(CheckInTime);
@@ -150,7 +279,6 @@
             Controls.Add(label2);
             Controls.Add(DatePicker);
             Controls.Add(label1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "RecordAttendanceForm";
             Text = "RecordAttendanceForm";
             Load += RecordAttendanceForm_Load;
@@ -168,7 +296,18 @@
         private Label label4;
         private DateTimePicker CheckInTime;
         private DateTimePicker CheckOutTime;
-        private Button dbtestbtn;
         private Button RecordAttendanceBtn;
+        private ListBox EmployeeListBox;
+        private Label label5;
+        private Label EmpIdLbl;
+        private Label EmpFirstNameLbl;
+        private Label EmpLastNameLbl;
+        private Label EmpEmailLbl;
+        private Label EmpPhoneLbl;
+        private Label EmpEmailFill;
+        private Label EmpPhoneFill;
+        private Label EmpLastNameFill;
+        private Label EmpFirstNameFill;
+        private Label EmpIdFill;
     }
 }
--- LinkHR/RecordAttendanceForm.cs
+++ LinkHR/RecordAttendanceForm.cs
@@ -4,11 +4,17 @@ namespace LinkHR
 {
     public partial class RecordAttendanceForm : Form
     {
+        private System.Windows.Forms.Timer searchTimer;
+
+        private bool suppressTextChanged = false;
+
         public RecordAttendanceForm()
         {
             InitializeComponent();
 
-            // Update DateTime pickers to work only as relevant picker type (date or time only)
+            searchTimer = new System.Windows.Forms.Timer();
+            searchTimer.Interval = 300;
+            searchTimer.Tick += SearchTimer_Tick;
 
             CheckInTime.Format = DateTimePickerFormat.Time;
             CheckInTime.ShowUpDown = true;
@@ -18,9 +24,13 @@ namespace LinkHR
 
             DatePicker.Format = DateTimePickerFormat.Short;
             DatePicker.ShowUpDown = false;
+
+            EmployeeListBox.Visible = false;
+
+            RecordAttendanceBtn.Enabled = false;
+
         }
 
-        // Function fot record attendance
         private void RecordAttendance(string empID, DateOnly date, TimeOnly checkIn, TimeOnly checkOut)
         {
             try
@@ -37,17 +47,15 @@ namespace LinkHR
                         cmd.Parameters.AddWithValue("@checkIn", checkIn);
                         cmd.Parameters.AddWithValue("@checkOut", checkOut);
 
-                       
-                        // Derive work hours
                         try
                         {
                             double hoursWorked = CalculateHoursWorked(checkIn, checkOut);
                             cmd.Parameters.AddWithValue("@hoursWorked", hoursWorked);
                         }
-                        catch(Exception ex)
+                        catch (Exception ex)
                         {
                             MessageBox.Show(ex.Message);
-                            return; 
+                            return;
                         }
 
                         int rowsAffected = cmd.ExecuteNonQuery();
@@ -62,10 +70,9 @@ namespace LinkHR
             }
         }
 
-        // Function for calculate work hours
         private double CalculateHoursWorked(TimeOnly chekIn, TimeOnly chekOut)
         {
-            // Validate check-in and check-out times
+
             if (chekOut > chekIn)
             {
                 TimeSpan duration = chekOut - chekIn;
@@ -79,7 +86,6 @@ namespace LinkHR
             }
         }
 
-        // Update attendance recordings
         private void RecordAttendanceBtn_Click(object sender, EventArgs e)
         {
             string empId = EmpIdTxt.Text;
@@ -91,10 +97,207 @@ namespace LinkHR
             RecordAttendance(empId, date, checkIn, checkOut);
         }
 
-        private void RecordAttendanceForm_Load(object sender, EventArgs e)
+        public class Employee
         {
+            public string id { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; }
 
+            public string Display => $"{id} \t {firstName} \t {lastName}";
         }
 
+        private List<Employee> SearchEmployees(string searchQuery)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using (SqlConnection conn = DBConnector.GetConnection())
+                {
+                    conn.Open();
+                    string query = "SELECT username, first_name, last_name, contact_no, email FROM Employee WHERE username LIKE @searchQuery OR first_name LIKE @searchQuery OR last_name LIKE @searchQuery";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@searchQuery", "%" + searchQuery + "%");
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            employees.Add(new Employee
+                            {
+                                id = reader["username"].ToString(),
+                                firstName = reader["first_name"].ToString(),
+                                lastName = reader["last_name"].ToString(),
+                                phone = reader["contact_no"].ToString(),
+                                email = reader["email"].ToString()
+                                
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error retrieving data: " + ex.Message);
+            }
+
+            return employees;
+        }
+
+        private void EmpIdTxt_TextChanged(object sender, EventArgs e)
+        {
+
+            if (suppressTextChanged) return;
+
+            RecordAttendanceBtn.Enabled = false;
+            ResetEmployeeDetails();
+
+            searchTimer.Stop();
+
+
+            searchTimer.Start();
+            
+        }
+
+
+        private void SearchTimer_Tick(object sender, EventArgs e)
+        {
+
+            searchTimer.Stop();
+
+            string searchQuery = EmpIdTxt.Text.Trim();
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                List<Employee> employees = SearchEmployees(searchQuery);
+
+                Console.WriteLine($"Found {employees.Count} employees.");
+
+                if (employees.Count > 0)
+                {
+                    EmployeeListBox.DataSource = employees;
+                    EmployeeListBox.DisplayMember = "Display";
+                    PositionListBoxUnderTextBox();
+                    EmployeeListBox.Visible = true;
+                }
+                else
+                {
+                    EmployeeListBox.DataSource = null;
+                    EmployeeListBox.Visible = false;
+                }
+            }
+            else
+            {
+                EmployeeListBox.DataSource = null;
+                EmployeeListBox.Visible = false;
+            }
+        }
+
+        private void EmployeeListBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            int index = EmployeeListBox.IndexFromPoint(e.Location);
+            if (index != ListBox.NoMatches)
+            {
+                var selectedEmployee = (Employee)EmployeeListBox.Items[index];
+
+                suppressTextChanged = true;
+                EmpIdTxt.Text = selectedEmployee.id;
+                suppressTextChanged = false;
+
+                EmployeeListBox.Visible = false;
+
+                RecordAttendanceBtn.Enabled = true;
+
+                ShowEmployeeDetails(selectedEmployee);
+            }
+        }
+        private void HideListBoxOnOutsideClick(object sender, EventArgs e)
+        {
+            Point cursor = this.PointToClient(Cursor.Position);
+            if (!EmployeeListBox.Bounds.Contains(cursor) && !EmpIdTxt.Bounds.Contains(cursor))
+            {
+                EmployeeListBox.Visible = false;
+
+                if (!IsValidEmployee(EmpIdTxt.Text))
+                {
+                    RecordAttendanceBtn.Enabled = false;
+                    ResetEmployeeDetails();
+                }
+            }
+        }
+
+        private void RecordAttendanceForm_Load(object sender, EventArgs e)
+        {
+            this.Controls.Add(EmployeeListBox);
+            EmployeeListBox.BringToFront();
+            this.Click += HideListBoxOnOutsideClick;
+            ResetEmployeeDetails();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (!EmployeeListBox.Bounds.Contains(PointToClient(MousePosition)) &&
+                !EmpIdTxt.Bounds.Contains(PointToClient(MousePosition)))
+            {
+                EmployeeListBox.Visible = false;
+            }
+        }
+
+        private void PositionListBoxUnderTextBox()
+        {
+            var textBoxLocation = EmpIdTxt.PointToScreen(Point.Empty);
+            var formLocation = this.PointToScreen(Point.Empty);
+            int x = textBoxLocation.X - formLocation.X;
+            int y = textBoxLocation.Y - formLocation.Y + EmpIdTxt.Height;
+
+            EmployeeListBox.Location = new Point(x, y);
+            EmployeeListBox.Width = EmpIdTxt.Width;
+        }
+
+        private void EmpIdTxt_Click(object sender, EventArgs e)
+        {
+            string searchQuery = EmpIdTxt.Text.Trim();
+            List<Employee> employees = SearchEmployees(searchQuery);
+
+            if (employees.Count > 0)
+            {
+                EmployeeListBox.DataSource = employees;
+                EmployeeListBox.DisplayMember = "Display";
+                PositionListBoxUnderTextBox();
+                EmployeeListBox.Visible = true;
+            }
+            else
+            {
+                EmployeeListBox.Visible = false;
+            }
+        }
+
+        private bool IsValidEmployee(string empId)
+        {
+            return SearchEmployees(empId).Exists(e => e.id.Equals(empId, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private void ShowEmployeeDetails(Employee selectedEmployee)
+        {
+            EmpIdFill.Text = selectedEmployee.id;
+            EmpFirstNameFill.Text = selectedEmployee.firstName;
+            EmpLastNameFill.Text = selectedEmployee.lastName;
+            EmpPhoneFill.Text = selectedEmployee.phone;
+            EmpEmailFill.Text = selectedEmployee.email;
+        }
+
+        private void ResetEmployeeDetails()
+        {
+            EmpIdFill.Text = "_";
+            EmpFirstNameFill.Text = "_";
+            EmpLastNameFill.Text = "_";
+            EmpPhoneFill.Text = "_";
+            EmpEmailFill.Text = "_";
+        }
     }
+
 }
