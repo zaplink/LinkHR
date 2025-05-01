2fa01162a7a9db1cf58245b6a9813a19e4eab9ba
UdasriHasindu <usrimart@gmail.com>
2025-05-01 12:00:19 +0530

    UI

--- LinkHR/RemoveEmployeeForm.cs
+++ LinkHR/RemoveEmployeeForm.cs
@@ -17,14 +17,8 @@ namespace LinkHR
             InitializeComponent();
         }
 
-        private void label4_Click(object sender, EventArgs e)
-        {
+        private void label4_Click(object sender, EventArgs e) {}
+        private void label5_Click(object sender, EventArgs e) {}
 
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
