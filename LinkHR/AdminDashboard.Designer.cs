70f5fa82c528ffd763669d4346a17e86c8f6cee4
UdasriHasindu <usrimart@gmail.com>
2025-05-01 17:17:33 +0530

    Add manager handling to admins

--- LinkHR/AdminDashboard.Designer.cs
+++ LinkHR/AdminDashboard.Designer.cs
@@ -40,11 +40,14 @@
             leavesToolStripMenuItem = new ToolStripMenuItem();
             reportsToolStripMenuItem = new ToolStripMenuItem();
             panel1 = new Panel();
-            adminPanel = new Panel();
-            label1 = new Label();
-            label2 = new Label();
-            panel2 = new Panel();
             logoutButton = new Button();
+            panel2 = new Panel();
+            label1 = new Label();
+            adminPanel = new Panel();
+            label2 = new Label();
+            managersToolStripMenuItem = new ToolStripMenuItem();
+            registerManagerToolStripMenuItem = new ToolStripMenuItem();
+            removeMToolStripMenuItem = new ToolStripMenuItem();
             menuStrip1.SuspendLayout();
             panel1.SuspendLayout();
             adminPanel.SuspendLayout();
@@ -54,7 +57,7 @@
             // 
             menuStrip1.BackColor = SystemColors.AppWorkspace;
             menuStrip1.ImageScalingSize = new Size(20, 20);
-            menuStrip1.Items.AddRange(new ToolStripItem[] { homeToolStripMenuItem, employeesToolStripMenuItem, attendenceToolStripMenuItem, leavesToolStripMenuItem, reportsToolStripMenuItem });
+            menuStrip1.Items.AddRange(new ToolStripItem[] { homeToolStripMenuItem, employeesToolStripMenuItem, managersToolStripMenuItem, attendenceToolStripMenuItem, leavesToolStripMenuItem, reportsToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Padding = new Padding(6, 3, 0, 3);
@@ -79,20 +82,23 @@
             // detailsToolStripMenuItem
             // 
             detailsToolStripMenuItem.Name = "detailsToolStripMenuItem";
-            detailsToolStripMenuItem.Size = new Size(216, 26);
+            detailsToolStripMenuItem.Size = new Size(224, 26);
             detailsToolStripMenuItem.Text = "Register Employee";
+            detailsToolStripMenuItem.Click += detailsToolStripMenuItem_Click;
             // 
             // removeEmployeeToolStripMenuItem
             // 
             removeEmployeeToolStripMenuItem.Name = "removeEmployeeToolStripMenuItem";
-            removeEmployeeToolStripMenuItem.Size = new Size(216, 26);
+            removeEmployeeToolStripMenuItem.Size = new Size(224, 26);
             removeEmployeeToolStripMenuItem.Text = "Remove Employee";
+            removeEmployeeToolStripMenuItem.Click += removeEmployeeToolStripMenuItem_Click;
             // 
             // viewEmployeeToolStripMenuItem
             // 
             viewEmployeeToolStripMenuItem.Name = "viewEmployeeToolStripMenuItem";
-            viewEmployeeToolStripMenuItem.Size = new Size(216, 26);
+            viewEmployeeToolStripMenuItem.Size = new Size(224, 26);
             viewEmployeeToolStripMenuItem.Text = "View Employee";
+            viewEmployeeToolStripMenuItem.Click += viewEmployeeToolStripMenuItem_Click;
             // 
             // attendenceToolStripMenuItem
             // 
@@ -106,24 +112,28 @@
             recordAttendanceToolStripMenuItem.Name = "recordAttendanceToolStripMenuItem";
             recordAttendanceToolStripMenuItem.Size = new Size(219, 26);
             recordAttendanceToolStripMenuItem.Text = "Record Attendance";
+            recordAttendanceToolStripMenuItem.Click += recordAttendanceToolStripMenuItem_Click;
             // 
             // viewAttendanceToolStripMenuItem
             // 
             viewAttendanceToolStripMenuItem.Name = "viewAttendanceToolStripMenuItem";
             viewAttendanceToolStripMenuItem.Size = new Size(219, 26);
             viewAttendanceToolStripMenuItem.Text = "View Attendance";
+            viewAttendanceToolStripMenuItem.Click += viewAttendanceToolStripMenuItem_Click;
             // 
             // leavesToolStripMenuItem
             // 
             leavesToolStripMenuItem.Name = "leavesToolStripMenuItem";
             leavesToolStripMenuItem.Size = new Size(67, 24);
             leavesToolStripMenuItem.Text = "Leaves";
+            leavesToolStripMenuItem.Click += leavesToolStripMenuItem_Click;
             // 
             // reportsToolStripMenuItem
             // 
             reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
             reportsToolStripMenuItem.Size = new Size(74, 24);
             reportsToolStripMenuItem.Text = "Reports";
+            reportsToolStripMenuItem.Click += reportsToolStripMenuItem_Click;
             // 
             // panel1
             // 
@@ -137,45 +147,6 @@
             panel1.Size = new Size(250, 723);
             panel1.TabIndex = 2;
             // 
-            // adminPanel
-            // 
-            adminPanel.Controls.Add(label2);
-            adminPanel.Dock = DockStyle.Fill;
-            adminPanel.Location = new Point(250, 30);
-            adminPanel.Name = "adminPanel";
-            adminPanel.Size = new Size(1082, 723);
-            adminPanel.TabIndex = 3;
-            // 
-            // label1
-            // 
-            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
-            label1.Location = new Point(38, 47);
-            label1.Name = "label1";
-            label1.Size = new Size(155, 30);
-            label1.TabIndex = 1;
-            label1.Text = "Team ZapLink";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 15F);
-            label2.Location = new Point(46, 42);
-            label2.Name = "label2";
-            label2.Size = new Size(135, 35);
-            label2.TabIndex = 4;
-            label2.Text = "Welcome..!";
-            // 
-            // panel2
-            // 
-            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            panel2.BackgroundImage = Properties.Resources.admin;
-            panel2.Location = new Point(50, 113);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(120, 120);
-            panel2.TabIndex = 2;
-            // 
             // logoutButton
             // 
             logoutButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
@@ -188,6 +159,64 @@
             logoutButton.Text = "Logout";
             logoutButton.UseVisualStyleBackColor = false;
             logoutButton.Click += logoutButton_Click;
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.BackgroundImage = Properties.Resources.admin;
+            panel2.Location = new Point(50, 113);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(120, 120);
+            panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
+            label1.Location = new Point(38, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(155, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Team ZapLink";
+            // 
+            // adminPanel
+            // 
+            adminPanel.Controls.Add(label2);
+            adminPanel.Dock = DockStyle.Fill;
+            adminPanel.Location = new Point(250, 30);
+            adminPanel.Name = "adminPanel";
+            adminPanel.Size = new Size(1082, 723);
+            adminPanel.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.Location = new Point(46, 42);
+            label2.Name = "label2";
+            label2.Size = new Size(135, 35);
+            label2.TabIndex = 4;
+            label2.Text = "Welcome..!";
+            // 
+            // managersToolStripMenuItem
+            // 
+            managersToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { registerManagerToolStripMenuItem, removeMToolStripMenuItem });
+            managersToolStripMenuItem.Name = "managersToolStripMenuItem";
+            managersToolStripMenuItem.Size = new Size(88, 24);
+            managersToolStripMenuItem.Text = "Managers";
+            // 
+            // registerManagerToolStripMenuItem
+            // 
+            registerManagerToolStripMenuItem.Name = "registerManagerToolStripMenuItem";
+            registerManagerToolStripMenuItem.Size = new Size(224, 26);
+            registerManagerToolStripMenuItem.Text = "Register Manager";
+            // 
+            // removeMToolStripMenuItem
+            // 
+            removeMToolStripMenuItem.Name = "removeMToolStripMenuItem";
+            removeMToolStripMenuItem.Size = new Size(224, 26);
+            removeMToolStripMenuItem.Text = "Remove Manager";
             // 
             // AdminDashboard
             // 
@@ -228,5 +257,8 @@
         private Panel panel2;
         private Label label2;
         private Button logoutButton;
+        private ToolStripMenuItem managersToolStripMenuItem;
+        private ToolStripMenuItem registerManagerToolStripMenuItem;
+        private ToolStripMenuItem removeMToolStripMenuItem;
     }
 }
